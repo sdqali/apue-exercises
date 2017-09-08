bbd6858aaa1e16a52c86ead75e834e8bbdd121da
Sadique Ali <sadiqalikm@gmail.com>
2017-09-07 21:55:25 -0700

    Default dir to PWD.

--- ls.c
+++ ls.c
@@ -4,12 +4,13 @@
 int main(int argc, char* argv[]) {
     DIR *dp;
     struct dirent *dirp;
+    char* directory_name = ".";
 
-    if(argc != 2) {
-      err_quit("Usage: ls <directory name>");
+    if(argc == 2) {
+      directory_name = argv[1];
     }
 
-    if((dp = opendir(argv[1])) == NULL) {
+    if((dp = opendir(directory_name)) == NULL) {
       err_sys("Can't open %s", argv[1]);
     }
 
