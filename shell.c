9863bbf115b981bc74b6bbc9e93f09a12e17f5fc
Sadique Ali <sadiqalikm@gmail.com>
2017-09-08 06:16:07 -0700

    Shell commands understand parameters.

--- shell.c
+++ shell.c
@@ -1,4 +1,31 @@
+#include <string.h>
 #include "apue.h"
+
+typedef struct command {
+  char *program;
+  char **args;
+} command;
+
+struct command split(char *input, char *delimiter) {
+  struct command output;
+  char *args[MAXLINE];
+  char *token, *string, *tofree;
+
+  tofree = string = strdup(input);
+
+  int i = 0;
+  while ((token = strsep(&string, delimiter)) != NULL) {
+    if(i == 0) {
+      output.program = token;
+    }
+    args[i] = token;
+    i++;
+  }
+  output.args = args;
+
+  free(tofree);
+  return output;
+}
 
 int main(void) {
   char buf[MAXLINE];
@@ -11,11 +38,13 @@ int main(void) {
       buf[strlen(buf) - 1] = 0;
     }
 
+    struct command cmd = split(buf, " \t");
+
     if ((pid = fork()) < 0) {
       err_sys("fork error");
     } else if (pid == 0) {
-      execlp(buf, buf, (char*) 0);
-      err_ret("Could not execute: %s", buf);
+      execvp(cmd.program, cmd.args);
+      err_ret("Could not execute: %s", cmd.program);
       exit(127);
     }
 
